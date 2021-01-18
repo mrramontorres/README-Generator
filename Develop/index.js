8011afa25bc9078316b89a0b4e2fbf97d6c5b8e1
turtles <me@ramon.nyc>
2021-01-18 14:16:06 -0800

    added inquirer question ingestion

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,65 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+//const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Enter project title:',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Enter project description:',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Enter installation instructions:',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'Enter usage information:',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Enter contribution guidelines:',
+      name: 'contribution',
+    },
+    {
+      type: 'input',
+      message: 'Enter test instructions:',
+      name: 'test',
+    },
+  ])
+  .then(() =>
+      console.log('Success!')
+  );
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
+
+
+// fs.writeFile('log.txt', process.argv[2], (err) =>
+//   err ? console.error(err) : console.log('Success!')
+// );
+
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
+
+
+
+
+
+
